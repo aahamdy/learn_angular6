ec3c9738439c9f053d8ab8ae0d11e9e699dc0d2e
Amr Hamdy <eng.aahamdy@gmail.com>
2018-07-12 17:02:07 +0200

    change observer to subject

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,26 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
 import { Ingredient } from '../shared/ingredient.model';
 import { ShopingListService } from './shoping-list.services';
+
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
-export class ShopingListComponent implements OnInit {
+export class ShopingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShopingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredient();
-    this.slService.ingredientsChanged
+    this.subscription = this.slService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
