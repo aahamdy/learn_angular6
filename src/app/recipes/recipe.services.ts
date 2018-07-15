adc9a03928b2c2168df87f007e074929df9adb1a
Amr Hamdy <eng.aahamdy@gmail.com>
2018-07-15 15:38:50 +0200

    add / edit recipes + add ingredients for each recipe

--- src/app/recipes/recipe.services.ts
+++ src/app/recipes/recipe.services.ts
@@ -3,11 +3,14 @@ import { Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShopingListService } from "../shoping-list/shoping-list.services";
+import { Subject } from "rxjs";
 
 @Injectable() 
 
 export class RecipeService {
     
+    recipesChanged = new Subject<Recipe[]>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a simply test', 'https://www.bbcgoodfood.com/sites/default/files/recipe/recipe-image/2016/09/one-pan-spaghetti-with-nduja-fennel-olives.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
         new Recipe('The Second Recipe', 'This is a simply description for the second recipe', 'https://www.bbcgoodfood.com/sites/default/files/recipe/recipe-image/2016/09/one-pan-spaghetti-with-nduja-fennel-olives.jpg',[new Ingredient('Buns', 2), new Ingredient('Meat', 1)])
@@ -25,6 +28,21 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe; 
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
     }
 
 }
